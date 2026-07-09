df57ac78252fd2517728d77a21f751b8f6d9e5c2
Abdalrahman Taher <bedotaher2005@gmail.com>
2026-07-09 03:07:49 +0300

    split into the four Blade files

--- resources/views/home/left-sidebar.blade.php
+++ resources/views/home/left-sidebar.blade.php
@@ -44,7 +44,7 @@
             <a href="#" class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-800 hover:text-blue-300">
                 <svg class="mr-4 h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
                     <path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
-                </svg>
+                            <a href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full bg-gray-800 text-blue-300">
                 Bookmarks
             </a>
 
